3f1917d7a896b67cc167b1394360b3a9e43bb472
TrongLam <mrlam99n01@gmail.com>
2019-05-14 19:19:38 +0700

    reducers Redux

---
+++ reducers/orders.js
@@ -0,0 +1,13 @@
+import * as actionTypes from '../actions/actionTypes';
+
+const initialState = {
+    orderData: [],
+    loading: false,
+    error: null
+}
+
+const reducer = (state = initialState, action) => {
+    return state;
+}
+
+export default reducer;
